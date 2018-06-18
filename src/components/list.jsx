54a961c26c53a13aa92306dfdd7692c7d455ee3d
busyok-bedlam <busyokbedlam@gmail.com>
2018-06-19 00:07:10 +0300

    4 commit

--- src/components/list.jsx
+++ src/components/list.jsx
@@ -8,12 +8,24 @@ class List extends Component {
   render() {
     const clients = this.props.clientsList;
     const formStatus = this.props.formStatus;
-    const isClients = clients.length > 0 && clients.map( (client,index) => <ListItem client={client} key={index} />);
+    const loader = (
+      <div className="ui container">
+        <div className="ui active dimmer">
+          <div className="ui text loader">Loading</div>
+        </div>
+        <p></p>
+      </div>
+    )
+    const isClients = clients.length > 0 ;
     return (
       <div >
         <h2 class="ui header">Clients List </h2>
         <div className="ui divided items">
-        { isClients }
+        {
+          isClients ?
+            clients.map( (client,index) => <ListItem client={client} key={index} />) :
+            loader
+        }
         </div>
 
       </div>
